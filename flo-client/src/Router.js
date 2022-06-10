471c3309717025bc9f848505fb5fa497d986750f
irene <irenecagnoni91@gmail.com>
2022-06-10 22:30:45 +0200

    fix(Root): remove redundant app route

--- flo-client/src/Router.js
+++ flo-client/src/Router.js
@@ -1,14 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
-import App from './App.js';
 import Journey from './pages/Journey';
 
 const Router = ({ journeys }) => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<App />} />
-        <Route path='/journeys' element={<Dashboard journeys={journeys} />} />
+        <Route path='/' element={<Dashboard journeys={journeys} />} />
         <Route path='/journeys/:id' element={<Journey />} />
       </Routes>
     </BrowserRouter>
