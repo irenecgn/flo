2a257a8aac9721634976d5f118958f25a25df2db
nicolaspfigueroa <nicolaspfigueroa@gmail.com>
2022-06-17 17:39:16 -0600

    after help with juan

--- flo-client/src/App.tsx
+++ flo-client/src/App.tsx
@@ -1,7 +1,8 @@
+import React from 'react';
 import Router from './Router';
 
 // On this file you want to add shared behaviours or context providers to make them available to the whole app (redux provider, etc..)
-function App() {
+const App: React.FC = () => {
   return <Router />;
 }
 
--- flo-client/src/Router.tsx
+++ flo-client/src/Router.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Journey from './pages/Journey';
