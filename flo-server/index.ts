ffb924cb3789358d861e1029a993497f470c3ded
PeicoPaco <rodrigo.reategui.g@gmail.com>
2022-06-20 17:54:23 -0500

    a test for the test

--- flo-server/index.ts
+++ flo-server/index.ts
@@ -1,20 +1,24 @@
-const Express = require('express');
-const app = new Express();
-const router = require('./router/router');
+const express = require('express');
+const app = new express();
+const Router = require('./router/router');
 const cors = require('cors');
 
+const {NODE_ENV} = process.env;
 const PORT = 3030; //TODO: get port from environment variable (NPM dotenv)
-const DB_URL = 'mongodb://localhost:27017/flo';
+const DB_URL = NODE_ENV === 'test' 
+ ? "mongodb://localhost:27017/test" 
+ : "mongodb://localhost:27017/flo" 
 
 const mongoose = require('mongoose');
 mongoose.connect(DB_URL).then(() => {
   console.log('Connected to DB 🗺');
 });
 
-app
-  .use(cors())
-  .use(Express.json())
-  .use(router)
-  .listen(PORT, () => {
+app.use(cors())
+app.use(express.json())
+app.use(Router)
+const server =  app.listen(PORT, () => {
     console.log(`Server listen on port ${PORT} 🗺`);
   });
+
+module.exports = {app, server}
